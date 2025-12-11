ade82678c76ae86fc66c20b060f77a5180f084a4
PetraAdrian <152401072+petraadrr@users.noreply.github.com>
2025-12-11 10:57:31 +0700

    Update response message in index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Aplikasi PaaS pertama saya berjalan di Render!");
+  res.send("Aplikasi PaaS pertama Kelompok 5 berjalan di Render!");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server berjalan di port ${port}`));
+
