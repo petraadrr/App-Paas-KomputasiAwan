c9fcf4d8f6f6b3e51736bccc0b4bd279368ef145
PetraAdrian <152401072+petraadrr@users.noreply.github.com>
2025-12-11 11:20:40 +0700

    Update response message for the root route

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Aplikasi PaaS pertama Kelompok 5 berjalan di Render!");
+  res.send("Aplikasi PaaS pertama Kelompok 5 berjalan di Railway!");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server berjalan di port ${port}`));
+
 
